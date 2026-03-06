5fa8133ab9d35c07c99c93c97ea01dba95c8a0e7
dipanshu kale <dipanshukale2003@gmail.com>
2026-03-06 11:20:05 +0530

    ready for deployment

--- ai-agent/src/services/api.ts
+++ ai-agent/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://backend-learning-v1.onrender.com/api/v1",
     withCredentials: true 
 });
 
