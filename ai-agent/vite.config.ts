6e5fa9861781b68aca755d8dc2773a7b17dc31c5
dipanshu kale <dipanshukale2003@gmail.com>
2026-03-17 15:42:03 +0530

    working on PWA

--- ai-agent/vite.config.ts
+++ ai-agent/vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+  manifest: {
+    name: "AI Agent",
+    short_name: "AI Agent",
+    theme_color: "#020617",
+    background_color: "#020617",
+    display: "standalone",
+    start_url: "/",
+    icons: [
+      {
+        src: "/icon-1.jpg",
+        sizes: "192x192",
+        type: "image/jpg/png"
+      },
+      {
+        src: "/icon-2.jpg",
+        sizes: "512x512",
+        type: "image/jpg/png"
+      }
+    ]
+  }
+})
+  ]
 })
