3461ebc9dfe4d4e0d74e65e2d7ac56993c481488
dipanshu kale <dipanshukale2003@gmail.com>
2026-03-17 18:21:10 +0530

    pwa is working now

--- ai-agent/src/App.tsx
+++ ai-agent/src/App.tsx
@@ -1,10 +1,21 @@
-import InstallPopup from "./components/installPopup.tsx";
+import { useState, useEffect } from "react";
+import SplashScreen from "./components/SplashScreen.tsx";
 import ChatWindow from "./components/chatWindow.tsx";
+import InstallPopup from "./components/installPopup.tsx";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000); // 2 sec splash
+  }, []);
+
   return (
     <>
-      <InstallPopup />
+      {loading && <SplashScreen />}
+      <InstallPopup/>
       <div className="h-screen bg-[#020617] flex items-center justify-center">
         <ChatWindow />
       </div>
